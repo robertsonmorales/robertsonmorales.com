5d601c0d6fbeb29111f2205761c801112ed02d84
robertsonmorales <58919933+robertsonmorales@users.noreply.github.com>
2023-08-08 01:12:00 +0000

    fixing skilss and expertise

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,45 +1,45 @@
-import HTML from "../images/svg/skills/html.svg";
-import CSS3 from "../images/svg/skills/css3.svg";
-import Sass from "../images/svg/skills/sass.svg";
-import JavaScript from "../images/svg/skills/javascript.svg";
-import JQuery from "../images/svg/skills/jquery.svg";
-import Bootstrap from "../images/svg/skills/bootstrap.svg";
-import VueJS from "../images/svg/skills/vue.svg";
-import PHP from "../images/svg/skills/php.svg";
-import MySQL from "../images/svg/skills/mysql.svg";
-import Laravel from "../images/svg/skills/laravel.svg";
-import Git from "../images/svg/skills/git.svg";
-import Figma from "../images/svg/skills/figma-1.svg";
-import Wix from "../images/svg/skills/wix.svg";
-import OutSystems from "../images/svg/skills/outsystems.svg";
-import MongoDB from "../images/svg/skills/mongodb.svg";
-import NodeJS from "../images/svg/skills/nodejs.svg";
-import ExpressJS from "../images/svg/skills/expressjs.svg";
-import Firebase from "../images/svg/skills/firebase.svg";
-import ReactJS from "../images/svg/skills/reactjs.svg";
-import PostgreSQL from "../images/svg/skills/postgreSQL.svg";
+import HTML from "../images/skills/html.svg";
+import CSS3 from "../images/skills/css3.svg";
+import Sass from "../images/skills/sass.svg";
+import JavaScript from "../images/skills/javascript.svg";
+import JQuery from "../images/skills/jquery.svg";
+import Bootstrap from "../images/skills/bootstrap.svg";
+import VueJS from "../images/skills/vue.svg";
+import PHP from "../images/skills/php.svg";
+import MySQL from "../images/skills/mysql.svg";
+import Laravel from "../images/skills/laravel.svg";
+import Git from "../images/skills/git.svg";
+import Figma from "../images/skills/figma.svg";
+import Wix from "../images/skills/wix.svg";
+import OutSystems from "../images/skills/outsystems.svg";
+import MongoDB from "../images/skills/mongodb.svg";
+import NodeJS from "../images/skills/nodejs.svg";
+import ExpressJS from "../images/skills/expressjs.svg";
+import Firebase from "../images/skills/firebase.svg";
+import ReactJS from "../images/skills/reactjs.svg";
+import PostgreSQL from "../images/skills/postgreSQL.svg";
 
 const skills = [
     { name: 'HTML 5', image: HTML},
-    // { name: 'CSS 3', image: CSS3},
-    // { name: 'Sass', image: Sass},
-    // { name: 'JavaScript', image: JavaScript},
-    // { name: 'JQuery', image: JQuery},
-    // { name: 'Bootstrap', image: Bootstrap},
-    // { name: 'VueJS', image: VueJS},
-    // { name: 'PHP', image: PHP},
-    // { name: 'MySQL', image: MySQL},
-    // { name: 'Laravel', image: Laravel},
-    // { name: 'Git', image: Git},
-    // // { name: 'Figma', image: Figma},
-    // { name: 'Wix', image: Wix},
-    // { name: 'OutSystems', image: OutSystems},
-    // { name: 'MongoDB', image: MongoDB},
-    // { name: 'NodeJS', image: NodeJS},
-    // { name: 'ExpressJS', image: ExpressJS},
-    // { name: 'Firebase', image: Firebase},
-    // { name: 'ReactJS', image: ReactJS},
-    // { name: 'PostgreSQL', image: PostgreSQL}
+    { name: 'CSS 3', image: CSS3},
+    { name: 'Sass', image: Sass},
+    { name: 'JavaScript', image: JavaScript},
+    { name: 'JQuery', image: JQuery},
+    { name: 'Bootstrap', image: Bootstrap},
+    { name: 'VueJS', image: VueJS},
+    { name: 'PHP', image: PHP},
+    { name: 'MySQL', image: MySQL},
+    { name: 'Laravel', image: Laravel},
+    { name: 'Git', image: Git},
+    { name: 'Figma', image: Figma},
+    { name: 'Wix', image: Wix},
+    { name: 'OutSystems', image: OutSystems},
+    { name: 'MongoDB', image: MongoDB},
+    { name: 'NodeJS', image: NodeJS},
+    { name: 'ExpressJS', image: ExpressJS},
+    { name: 'Firebase', image: Firebase},
+    { name: 'ReactJS', image: ReactJS},
+    { name: 'PostgreSQL', image: PostgreSQL}
     // Add more skills here
 ];
 
